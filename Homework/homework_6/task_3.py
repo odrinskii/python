1e3b6bd0baa7aa2a27aec25760db3c806906993d
odrinskii <ananjev.arseniy@gmail.com>
2023-01-22 23:39:30 +0300

    practice and homework

--- Homework/homework_6/task_3.py
+++ Homework/homework_6/task_3.py
@@ -3,3 +3,18 @@
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
 
+
+def divrecurs(x, y, result):
+    if y == 1:
+        return result
+    return divrecurs(x, y - 1, result * x)
+
+
+a, b = int(input('Введите первое число: ')), \
+    int(input('Введите второе число: '))
+res = a
+print(f"Результат: {divrecurs(a, b, res)}")
+
+
+
+
